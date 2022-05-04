0e22ecfd24e928e343a4a9ee5e373ac0a10e3a83
Amir Tuval <amirtu@codota.com>
2022-05-04 15:44:05 +0300

    support linux musl

--- cross/build-all-cross-images.sh
+++ cross/build-all-cross-images.sh
@@ -6,3 +6,4 @@ set -e
 cd "$(dirname "$0")"
 
 docker build . -f x86_64-unknown-linux-gnu/Dockerfile -t tch-rs/x86_64-unknown-linux-gnu
+docker build . -f x86_64-unknown-linux-musl/Dockerfile -t tch-rs/x86_64-unknown-linux-musl
